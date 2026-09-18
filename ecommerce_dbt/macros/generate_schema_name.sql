aa428adf4cbde178a9c88cd49aab8c577e4ca845
DHANA5982 <sekar.dhana5982@gmail.com>
2026-09-18 15:40:23 +0100

    Add environment-specific dbt schema configuration

--- ecommerce_dbt/macros/generate_schema_name.sql
+++ ecommerce_dbt/macros/generate_schema_name.sql
@@ -1,9 +1,19 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- if custom_schema_name is none -%}
+
         {{ target.schema }}
 
+    {%- elif target.name == 'ci' -%}
+
+        CI_{{ custom_schema_name | trim }}
+
+    {%- elif target.name == 'prod' -%}
+
+        PROD_{{ custom_schema_name | trim }}
+
     {%- else -%}
+
         {{ custom_schema_name | trim }}
 
     {%- endif -%}
